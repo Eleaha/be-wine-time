d027248b553a8e929cb571b7f3f7b714049f0886
Eleaha <ellieharrington8@gmail.com>
2024-06-23 17:45:59 +0100

    add GET /api/users/:user_id/recipes with error handling

--- src/controllers/recipes-controllers.ts
+++ src/controllers/recipes-controllers.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { Recipe } from "../interfaces";
-import { fetchRecipes, fetchRecipeById } from "../models/recipes-model";
+import {
+	fetchRecipes,
+	fetchRecipeById,
+	fetchRecipesByUserId,
+} from "../models/recipes-model";
 
 export const getRecipes = async (
 	req: Request,
@@ -26,6 +30,20 @@ export const getRecipeById = async (
 		res.status(200).send({ recipe });
 	} catch (err) {
 		console.log(err);
+		return next(err);
+	}
+};
+
+export const getRecipesByUserId = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const { user_id } = req.params;
+	try {
+		const recipes: Recipe[] = await fetchRecipesByUserId(+user_id);
+		res.status(200).send({ recipes });
+	} catch (err) {
 		return next(err);
 	}
 };
--- src/routes/users-router.ts
+++ src/routes/users-router.ts
@@ -1,14 +1,21 @@
 import express, { Router } from "express";
 import {
-  getUserById,
-  getUsers,
-  postBrewByUserId,
+	getUserById,
+	getUsers,
+	postBrewByUserId,
 } from "../controllers/users-controller";
 import { getBrewsByUserId } from "../controllers/brews-controller";
+import {
+	getRecipeById,
+	getRecipesByUserId,
+} from "../controllers/recipes-controllers";
 
 export const usersRouter: Router = express.Router();
 
 usersRouter.get("/", getUsers);
 usersRouter.get("/:user_id", getUserById);
+
 usersRouter.get("/:user_id/brews", getBrewsByUserId);
 usersRouter.post("/:user_id/brews", postBrewByUserId);
+
+usersRouter.get("/:user_id/recipes", getRecipesByUserId);
