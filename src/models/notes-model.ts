9aa7b58ea6fed2bc8805d6df4a6e16204eb26f21
Eleaha <ellieharrington8@gmail.com>
2024-06-29 20:47:40 +0100

    add PATCH /api/notes/:note_id

--- src/models/notes-model.ts
+++ src/models/notes-model.ts
@@ -2,47 +2,60 @@ import format from "pg-format";
 import { db } from "../db/db-connection";
 import { Brew, Note } from "../interfaces";
 import { fetchBrewById } from "./brews-model";
+import { formatSQLColumnString } from "../utils";
 
 export const fetchNoteById = async (noteId: number) => {
-    const { rows } = await db.query(
-        `
+	const { rows } = await db.query(
+		`
         SELECT * FROM notes WHERE id = $1`,
-        [noteId]
-    );
-    return !rows.length
-        ? Promise.reject({ status: 404, msg: "Not found" })
-        : rows[0];
+		[noteId]
+	);
+	return !rows.length
+		? Promise.reject({ status: 404, msg: "Not found" })
+		: rows[0];
 };
 
 export const fetchNotesByBrewId = async (brewId: number) => {
-    const { rows } = await db.query(
-        `
+	const { rows } = await db.query(
+		`
         SELECT * FROM notes WHERE maker_id = $1
         `,
-        [brewId]
-    );
-    if (!rows.length) {
-        const brew: Brew = await fetchBrewById(brewId);
-        return !brew
-            ? Promise.reject({ status: 404, msg: "Not found" })
-            : Promise.reject({ status: 404, msg: "No notes yet!" });
-    }
-    return rows;
+		[brewId]
+	);
+	if (!rows.length) {
+		const brew: Brew = await fetchBrewById(brewId);
+		return !brew
+			? Promise.reject({ status: 404, msg: "Not found" })
+			: Promise.reject({ status: 404, msg: "No notes yet!" });
+	}
+	return rows;
 };
 
 export const insertNote = async (payload: Note) => {
-    const cols: string[] = Object.keys(payload);
-    const values: any[] = Object.values(payload);
+	const cols: string[] = Object.keys(payload);
+	const values: any[] = Object.values(payload);
 
-    const colString = cols.join(", ");
-    const queryString: string = format(
-        `
+	const colString = cols.join(", ");
+	const queryString: string = format(
+		`
 		INSERT INTO notes (${colString})
 		VALUES %L
 		RETURNING *`,
-        [values]
-    );
-    const { rows } = await db.query(queryString);
-    return rows[0];
+		[values]
+	);
+	const { rows } = await db.query(queryString);
+	return rows[0];
+};
 
-}
+export const updateNoteById = async (
+	noteId: number,
+	updateObj: { [index: string]: any }
+) => {
+	const setString: string = formatSQLColumnString(updateObj);
+	const queryString = `UPDATE notes SET ${setString} WHERE id = $1 RETURNING *;`;
+
+	const { rows } = await db.query(queryString, [noteId]);
+	return !rows.length
+		? Promise.reject({ status: 404, msg: "Not found" })
+		: rows[0];
+};
--- src/models/recipes-model.ts
+++ src/models/recipes-model.ts
@@ -5,79 +5,79 @@ import { formatSQLColumnString } from "../utils";
 import { fetchUserById } from "./users-model";
 
 export const fetchRecipes = async () => {
-    const { rows } = await db.query(`SELECT * FROM recipes`);
-    return rows;
+	const { rows } = await db.query(`SELECT * FROM recipes`);
+	return rows;
 };
 
 export const fetchRecipeById = async (recipeId: number) => {
-    const { rows } = await db.query(
-        `
+	const { rows } = await db.query(
+		`
         SELECT * FROM recipes
         WHERE id = $1
         `,
-        [recipeId]
-    );
+		[recipeId]
+	);
 
-    return !rows.length
-        ? Promise.reject({ status: 404, msg: "Not found" })
-        : rows[0];
+	return !rows.length
+		? Promise.reject({ status: 404, msg: "Not found" })
+		: rows[0];
 };
 
 export const fetchRecipesByUserId = async (userId: number) => {
-    const { rows } = await db.query(
-        `
+	const { rows } = await db.query(
+		`
 	SELECT * FROM recipes
 	WHERE maker_id = $1
     `,
-        [userId]
-    );
-    if (!rows.length) {
-        const user: User = await fetchUserById(userId);
-        return user["id"] === userId
-            ? Promise.reject({ status: 404, msg: "No recipes yet!" })
-            : Promise.reject({ status: 404, msg: "Not Found" });
-    }
-    return rows;
+		[userId]
+	);
+	if (!rows.length) {
+		const user: User = await fetchUserById(userId);
+		return user["id"] === userId
+			? Promise.reject({ status: 404, msg: "No recipes yet!" })
+			: Promise.reject({ status: 404, msg: "Not Found" });
+	}
+	return rows;
 };
 
 export const insertRecipe = async (payload: Recipe) => {
-    const cols: string[] = Object.keys(payload);
-    const values: any[] = Object.values(payload);
+	const cols: string[] = Object.keys(payload);
+	const values: any[] = Object.values(payload);
 
-    const colString = cols.join(", ");
-    const queryString: string = format(
-        `
+	const colString = cols.join(", ");
+	const queryString: string = format(
+		`
         INSERT INTO recipes (${colString})
         VALUES %L
         RETURNING *
         `,
-        [values]
-    );
-    const { rows } = await db.query(queryString);
-    return rows[0];
+		[values]
+	);
+	const { rows } = await db.query(queryString);
+	return rows[0];
 };
 
 export const updateRecipeById = async (
-    recipeId: number,
-    updateObj: { [index: string]: any }
+	recipeId: number,
+	updateObj: { [index: string]: any }
 ) => {
-    const setString: string = formatSQLColumnString(updateObj);
-    const queryString = `UPDATE recipes SET ${setString} WHERE id = $1 RETURNING *;`;
+	const setString: string = formatSQLColumnString(updateObj);
+	const queryString = `UPDATE recipes SET ${setString} WHERE id = $1 RETURNING *;`;
 
-    const { rows } = await db.query(queryString, [recipeId]);
-    return !rows.length
-        ? Promise.reject({ status: 404, msg: "Not found" })
-        : rows[0];
+	const { rows } = await db.query(queryString, [recipeId]);
+	return !rows.length
+		? Promise.reject({ status: 404, msg: "Not found" })
+		: rows[0];
 };
 
 export const removeRecipeById = async (recipeId: number) => {
-    const { rows } = await db.query(
-        `
+	const { rows } = await db.query(
+		`
         DELETE FROM recipes WHERE id = $1 RETURNING *;
         `,
-        [recipeId]
-    );
-    if (!rows.length) {
-        return Promise.reject({ status: 404, msg: "Not found" });
-    }
+		[recipeId]
+	);
+	if (!rows.length) {
+		return Promise.reject({ status: 404, msg: "Not found" });
+	}
 };
